e5beabbb3fd3697daeadb1dca523b197fe14bbb8
Kira <krutskinakira@gmail.com>
2023-06-15 12:34:44 +0500

    refactor

---
+++ keyboard.py
@@ -0,0 +1,14 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove,InlineKeyboardButton,InlineKeyboardMarkup, ForceReply
+from aiogram import types
+
+
+new_cart_bt = KeyboardButton("Cоздать новую корзину")
+connect_cart_bt = KeyboardButton("Найти корзину")
+
+cart_kb = ReplyKeyboardMarkup(one_time_keyboard=False).add(new_cart_bt, connect_cart_bt)
+
+add_item_bt = KeyboardButton("Добавить элемент")
+remove_item_bt = KeyboardButton("Удалить элемент")
+show_item_bt = KeyboardButton("Показать список")
+
+item_kb = ReplyKeyboardMarkup(one_time_keyboard=False).add(add_item_bt, remove_item_bt).add(show_item_bt)
--- shopping_list.py
+++ shopping_list.py
@@ -1,136 +1,114 @@
-"""
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-# Замените "YOUR_BOT_TOKEN" на токен вашего бота
-TOKEN = ""
-
-# Создание экземпляров Bot и Dispatcher
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
-
-# Словарь для хранения списка покупок
-shopping_list = []
-
-# Обработчик команды /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Привет! Я бот для создания списка покупок. Напиши /add, чтобы добавить элемент в список.")
-
-# Обработчик команды /add
-@dp.message_handler(commands=['add'])
-async def add_item(message: types.Message):
-    await message.reply("Введите элемент для добавления в список покупок:")
-    dp.register_message_handler(handle_new_item, state="*")  # Регистрация обработчика для следующего сообщения
-
-# Обработчик нового элемента списка
-async def handle_new_item(message: types.Message):
-    new_item = message.text
-    shopping_list.append(new_item)
-
-    await message.reply(f"Элемент '{new_item}' успешно добавлен в список покупок.")
-
-
-@dp.message_handler(commands=['delete'])
-async def delete_item(message: types.Message, state: FSMContext):
-    await message.reply("Введите элемент для удаления из списка покупок:")
-    old_item = message.text
-    shopping_list.remove(old_item)
-    await state.set_state("enter_to_delete")
-
-
-@dp.message_handler(state="enter_to_delete")
-async def handle_old_item(message: types.Message):
-    old_item = message.text
-    shopping_list.remove(old_item)
-    await message.reply(f"Элемент '{old_item}' успешно удалён из списка покупок.")
-    await state.set_state("be nice")
-
-@dp.message_handler(state="be nice")
-async def handle_old_item(message: types.Message):
-    await message.reply("You are veeeeery cute!")
-
-# Обработчик команды /list
-@dp.message_handler(commands=['list'])
-async def show_list(message: types.Message):
-    if not shopping_list:
-        await message.reply("Список покупок пуст.")
-    else:
-        list_text = "\n".join(shopping_list)
-        await message.reply(f"Список покупок:\n{list_text}")    
-
-# Запуск бота
-if __name__ == '__main__':
-    asyncio.run(dp.start_polling())
-    """
 import asyncio
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+
+# импорт клавиатуры
+from keyboard import item_kb, cart_kb, new_cart_bt, connect_cart_bt, add_item_bt, remove_item_bt, show_item_bt
 
 TOKEN = "6160888297:AAH5KdnfEt0aPINbu8KwzwaQKhqJB3qjoAI"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-shopping_carts: dict[int, list] = dict()
-
+shopping_carts: list[list] = list() # список со списками покупок разных пользователей
+user_mapping: dict[int, int] = dict() # словарь id-пользователей(ключ) - id-корзины(значение)
 
 # Обработчик команды /start
 @dp.message_handler(commands=['start'], state="*")
 async def start(message: types.Message, state: FSMContext):
-    shopping_carts[message.from_id] = list()
-    await message.reply("Привет! Я бот для создания списка покупок. Напиши /add, чтобы добавить элемент в список.")
+    await message.reply("Привет! Я бот для создания списка покупок.\n"
+                        "Ты можешь создать новую корзину или присоединиться к уже существующей.",
+                        reply_markup=cart_kb)
+    
+@dp.message_handler(Text(equals=new_cart_bt.text), state="*")
+@dp.message_handler(commands=['new'], state="*")
+async def new(message: types.Message, state: FSMContext):
+    cart_id = len(shopping_carts)
+    cart = list()
+    shopping_carts.append(cart)
+    user_mapping[message.from_id] = cart_id
+    await message.reply(f"Вот id твоей корзины: {user_mapping[message.from_id]}."
+                        " Запомни его и никому не говори!!"
+                        " Если ты захочешь присоеденить свою семью, им понадобиться твой id.",
+                        reply_markup=item_kb)
+
+# Обработчик команды /connect
+@dp.message_handler(Text(equals=connect_cart_bt.text), state="*")
+@dp.message_handler(commands=['connect'], state="*")
+async def connect(message: types.Message, state: FSMContext):
+    await state.set_state("wait_cart_id")
+    await message.reply("Введите id корзины:")
+
+@dp.message_handler(state="wait_cart_id")
+async def enter_cart_id(message: types.Message, state: FSMContext):
+    cart_id = int(message.text)
+    if cart_id >= len(shopping_carts) or cart_id < 0:
+        await message.reply("Ой-ой (⌣̀_⌣́), такой корзины не сущестует...")
+    else:
+        user_mapping[message.from_id] = cart_id
+        await message.reply("Поздравляю, вы законектились с корзиной! \(★ω★)/", reply_markup=item_kb)
+    
+    await state.reset_state()
 
 # Обработчик команды /add
+@dp.message_handler(Text(equals=add_item_bt.text), state="*")
 @dp.message_handler(commands=['add'], state="*")
 async def add_item(message: types.Message, state: FSMContext):
     await message.reply("Введите элемент для добавления в список покупок:")
     await state.set_state("wait_item_to_add")
 
+# Обработчик команды /remove
+@dp.message_handler(Text(equals = remove_item_bt.text), state="*")
+@dp.message_handler(commands=['remove'], state = "*")
+async def remove_item(message: types.Message, state: FSMContext):
+    cart_id = user_mapping[message.from_id]
+    cart = shopping_carts[cart_id]
+    if not cart:
+        await message.reply("Список покупок ещё пуст((")
+    else:
+        await message.reply("Введите элемент для удаления:")
+        await state.set_state("wait_item_to_remove")
+
 # Обработчик нового элемента списка
 @dp.message_handler(state="wait_item_to_add")  
 async def handle_new_item(message: types.Message, state: FSMContext):
     new_item = message.text
-    shopping_cart = shopping_carts[message.from_id]
-    shopping_cart.append(new_item)
-    await message.reply(f"Элемент '{new_item}' успешно добавлен в список покупок!")
+    cart_id = user_mapping[message.from_id]
+    cart = shopping_carts[cart_id]
+    cart.append(new_item)
+    await message.reply(f"Элемент '{new_item}' добавлен в список покупок!")
     await state.reset_state(with_data=False)
 
-# Обработчик команды /remove
-# @dp.message_handler(commands=['remove'])
-# async def remove_item(message: types.Message):
-#     if not shopping_list:
-#         await message.reply("Список покупок пуст ((")
-#     else:
-#         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-#         buttons = [types.KeyboardButton(item) for item in shopping_list]
-#         keyboard.add(*buttons)
+#  Обработчик удаления элемента списка
+@dp.message_handler(state="wait_item_to_remove")  
+async def handle_remove_item(message: types.Message, state: FSMContext):
+    item_to_remove = message.text
+    cart_id = user_mapping[message.from_id]
+    cart = shopping_carts[cart_id]
+    if item_to_remove in cart:
+        cart.remove(item_to_remove)
+        await message.reply(f"Элемент '{item_to_remove}' удален из списка покупок.")
+    else:
+        await message.reply("Такого элемента нет в списке покупок.")
+    await state.reset_state(with_data=False)
 
-#         await message.reply("Выберите элемент для удаления:", reply_markup=keyboard)
-#         dp.register_message_handler(handle_remove_item, state="*")  # Регистрация обработчика для следующего сообщения
 
-# # Обработчик удаления элемента списка
-# async def handle_remove_item(message: types.Message):
-#     item_to_remove = message.text
-
-#     if item_to_remove in shopping_list:
-#         shopping_list.remove(item_to_remove)
-#         await message.reply(f"Элемент '{item_to_remove}' успешно удален из списка покупок.")
-#     else:
-#         await message.reply("Выбранный элемент не найден в списке покупок.")
-
+# Обработчик команды /list
+@dp.message_handler(Text(equals = show_item_bt.text), state="*")
 @dp.message_handler(commands=['list'], state="*")
 async def show_list(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    shopping_cart = shopping_carts[message.from_id]
+    cart_id = user_mapping[message.from_id]
+    cart = shopping_carts[cart_id]
 
-    if not shopping_cart:
-        await message.reply("Список покупок пуст.")
+# проверка пустоты списка
+    if not cart:
+        await message.reply("Список покупок ещё пуст. [± _ ±]")
     else:
-        list_text = "\n".join(shopping_cart)
-        await message.reply(f"Список покупок:\n{list_text}")
+        list_text = "\n".join(cart)
+        await message.reply(f"Вот список покупок <3:\n{list_text}")
 
 # Запуск бота
 if __name__ == '__main__':
